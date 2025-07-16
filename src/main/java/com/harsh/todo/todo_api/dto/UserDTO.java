518ab7288821e1a8e84b12ecc67d90f170f3a297
Harsh <Sharmahs812004@gmail.com>
2025-07-16 16:10:07 +0530

    Configured H2 database and added User entity, DTO, repository

---
+++ src/main/java/com/harsh/todo/todo_api/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.harsh.todo.todo_api.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/harsh/todo/todo_api/model/User.java
@@ -0,0 +1,22 @@
+package com.harsh.todo.todo_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role = "USER"; // default role
+}
